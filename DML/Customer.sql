fda892f5430b0d6b4782db156b25083a5f0521f7
xianggao3 <xiang.gao.1@stonybrook.edu>
2017-03-20 21:46:42 -0400

    fixed customer

--- DML/Customer.sql
+++ DML/Customer.sql
@@ -1,57 +1,44 @@
-<<<<<<< HEAD
 --- Add Account
 INSERT INTO Account(Id, DateOpened, Type, CustomerId)
-	Values(1, 10-1-06, umlimited-2, 444-44-4444);
+	Values(1, 10-1-06, 'umlimited-2', 444-44-4444);
 INSERT INTO Account(Id, DateOpened, Type, CustomerId)
-	Values(2, 10-15-06, limited, 222-22-2222);
+	Values(2, 10-15-06, 'limited', 222-22-2222);
 
 
-=======
->>>>>>> 1458ac6b3b877cafe20ce054afae168e0eb4f910
 --- 1.	A customer's currently held movies
 SELECT MovieId
 FROM Rental
 WHERE AccountId =1 AND EXISTS
 	SELECT ReturnDate
 	FROM Order
-	WHERE OrderId = Id AND ReturnDate > NOW()
+	WHERE OrderId = Id AND ReturnDate > NOW();
 ---2.	A customer's queue of movies it would like to see
 SELECT MovieId
 FROM MovieQ
-WHERE AccountId = 1
+WHERE AccountId = 1;
 ---3.	A customer's account settings
 SELECT *
 FROM Customer
-WHERE Id = 1
+WHERE Id = 1;
 ---4.	A history of all current and past orders a customer has placed
 SELECT Id 
 FROM Order
-WHERE DateTime <= NOW()
+WHERE DateTime <= NOW();
 ---5.	Movies available of a particular type
 SELECT *
 FROM Movies
-<<<<<<< HEAD
-WHERE Type=’Comedy’
+WHERE Type=’Comedy’;
 ---6.	Movies available with a particular keyword or set of keywords in the movie name
 SELECT * 
 FROM Movies
 WHERE Name LIKE ‘Godfather’ 
-	AND LIKE ‘Borat’
-=======
-WHERE Type=’Romance’
----6.	Movies available with a particular keyword or set of keywords in the movie name
-SELECT * 
-FROM Movies
-WHERE Name LIKE ‘keyword’ 
-	AND LIKE ‘keyword2’
->>>>>>> 1458ac6b3b877cafe20ce054afae168e0eb4f910
+	AND LIKE ‘Borat’;
 ---7.	Movies available starring a particular actor or group of actors
 SELECT MovieId 
 FROM AppearedIn, Actor
 WHERE ActorId=Id AND (
-<<<<<<< HEAD
  Name LIKE ‘Al Pacino’ 
-	OR Name LIKE ‘Tim Robbins’)
+	OR Name LIKE ‘Tim Robbins’);
 
 ---8.	Best-Seller list of movies
 ---SELECT TOP 10 MovieId
@@ -62,16 +49,3 @@ WHERE ActorId=Id AND (
 ---10.	Rate the movies they have rented
 ---UPDATE Movie
 ---SET 
-=======
- Name LIKE ‘actorName’ 
-	OR Name LIKE ‘actorName2’)
-
----8.	Best-Seller list of movies
-SELECT TOP 10 MovieId
-FROM Rental
-WHERE 
----9.	Personalized movie suggestion list
----10.	Rate the movies they have rented
-UPDATE Movie
-SET
->>>>>>> 1458ac6b3b877cafe20ce054afae168e0eb4f910
