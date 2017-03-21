1458ac6b3b877cafe20ce054afae168e0eb4f910
Marisa DePasquale <depasquale.marisa@gmail.com>
2017-03-20 21:37:34 -0400

    Deleted duplicate code

--- DML/CustomerRep.sql
+++ DML/CustomerRep.sql
@@ -71,7 +71,7 @@ WHERE Id = '111-11-1111';
 SELECT C.email
 FROM Customer C;
 
-#Record an order. -- Working --
+#Record an order. 
 
 #Order 1
 INSERT INTO MovieOrder(Id, AccountId, MovieId, DateAndTime, ReturnDate)
@@ -89,62 +89,7 @@ VALUES(3, '1', 3, '2009-11-12 9:30', NULL);
 INSERT INTO MovieOrder(Id, AccountId, MovieId, DateAndTime, ReturnDate)
 VALUES(4, '2', 2, '2009-11-21 22:22', NULL);
 
-###############################################################################
-#Add information for a customer -- Working --
-#Customer 1
-INSERT INTO Location(ZipCode, City, State)
-VALUES(11790, 'Stony Brook', 'NY');
-
-INSERT INTO Person(SSN,LastName,FirstName,Address,ZipCode,Telephone)
-VALUES('111-11-1111', 'Yang', 'Shang', '123 Success Street', 11790, '516-632-8959');
-
-INSERT INTO Customer(Id, Email, Rating, CreditCardNumber)
-VALUES('111-11-1111', 'syang@cs.sunysb.edu', 1, '1234-5678-1234-5678');
-
-#Customer 2
-INSERT INTO Person(SSN,LastName,FirstName,Address,ZipCode,Telephone)
-VALUES('222-22-2222', 'Du', 'Victor', '456 Fortune Road', 11790, '516-632-4360');
-
-INSERT INTO Customer(Id, Email, Rating, CreditCardNumber)
-VALUES('222-22-2222', 'vicdu@cs.sunysb.edu', 1, '5678-1234-5678-1234');
-
-#Customer 3
-INSERT INTO Location(ZipCode, City, State)
-VALUES(93536, 'Los Angeles', 'CA');
-
-INSERT INTO Person(SSN,LastName,FirstName,Address,ZipCode,Telephone)
-VALUES('333-33-3333', 'Smith', 'John', '789 Peace Blvd.', 93536, '315-443-4321');
-
-INSERT INTO Customer(Id, Email, Rating, CreditCardNumber)
-VALUES('333-33-3333', 'jsmith@ic.sunysb.edu', 1, '2345-6789-2345-6789');
-
-#Customer 4
-INSERT INTO Location(ZipCode, City, State)
-VALUES(11794, 'Stony Brook', 'NY');
-
-INSERT INTO Person(SSN,LastName,FirstName,Address,ZipCode,Telephone)
-VALUES('444-44-4444', 'Philip', 'Lewis', '135 Knowledge Lane', 11794, '516-666-8888');
-
-INSERT INTO Customer(Id, Email, Rating, CreditCardNumber)
-VALUES('444-44-4444', 'pml@cs.sunysb.edu', 1, '6789-2345-6789-2345');
-
 ################################################################################
-#Edit information for a customer -- Working --
-UPDATE Customer
-SET    Rating = 3
-WHERE  Id = '111-11-1111';
-
-#Delete information about a customer -- Working --
-DELETE FROM Customer
-WHERE Id = '111-11-1111';
-
-
-#Produce customer mailing lists -- Working -- 
-
-SELECT C.email
-FROM Customer C;
-
-###############################################################
 #Actor 1
 INSERT INTO Actor(Id, Name, Age, Sex, Rating)
 VALUES(1, 'Al Pacino', 63, 'M', 5);
