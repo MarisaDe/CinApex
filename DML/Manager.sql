eec35c1f61fa8523414f9a0008c0a6caf014dfaa
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-03-20 20:52:40 -0400

    added other test cases for adding movie and employee

--- DML/Manager.sql
+++ DML/Manager.sql
@@ -10,6 +10,19 @@ WHERE  Id = 1;
 DELETE FROM Movie
 WHERE Id = 1;
 
+----- OTHER DATA FOR Movie
+
+-- 2	Shawshank Redemption	Drama	1,000	2	4
+INSERT INTO Movie(Id, Name, Type, Rating, DistrFee, NumCopies )
+            VALUES 
+                 (2, "Shawshank Redemption", "Drama", 4, 1000, 2);
+
+-- 3	Borat	Comedy	500	1	3
+INSERT INTO Movie(Id, Name, Type, Rating, DistrFee, NumCopies )
+            VALUES 
+                 (3, "Borat", "Comedy", 3, 500, 1);
+-----
+
 --- Add, Edit and Delete information for an employee [TESTED AND WORKING]
 
 INSERT INTO Location(ZipCode, City, State)
@@ -22,14 +35,31 @@ INSERT INTO Person(SSN, LastName, FirstName, Address, ZipCode, Telephone)
 
 INSERT INTO Employee(Id, SSN, StartDate, HourlyRate)
                 VALUES
-                    (2, "123-45-6789", 20151105, 60);
+                    (1, "123-45-6789", 20151105, 60);
 
 UPDATE Employee
 SET    HourlyRate = 65
-WHERE  Id = 2;
+WHERE  Id = 1;
 
 DELETE FROM Employee
-WHERE       Id = 2;
+WHERE       Id = 1;
+
+----- OTHER EMPLOYEE
+
+-- 789-12-3456	Warren	David	456 Sunken Street	Stony Brook	NY	11794	631-632-9987	2-2-06	$50
+
+INSERT INTO Location(ZipCode, City, State)
+            VALUES
+            (11794, "Stony Brook", "NY");
+
+INSERT INTO Person(SSN, LastName, FirstName, Address, ZipCode, Telephone)
+                VALUES 
+                    ("789-12-3456", "Warren", "David", "456 Sunken Street", "11794", "631-632-9987");
+
+INSERT INTO Employee(Id, SSN, StartDate, HourlyRate)
+                VALUES
+                    (2, "789-12-3456", 20060202, 50);
+-----
 
 --- Obtain a sales report (i.e. the overall income from all active subscriptions) for a particular month
 
@@ -37,7 +67,7 @@ CREATE VIEW monthlyReport(Id, DistrFee) AS
     SELECT  M.Id, M.DistrFee
     FROM    Movie M, MovieOrder MO
     WHERE   M.Id = MO.Id AND
-            MONTH(DateTime) = 3 ;        --- MARCH
+            MONTH(DateTime) = 3;        --- MARCH
 
 SELECT SUM(DistrFee)
 FROM   monthlyReport;
