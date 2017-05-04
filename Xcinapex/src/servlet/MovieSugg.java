67ef8f1801b2f04cf574b148682d238eaec7cee1
xianggao3 <xiang.gao.1@stonybrook.edu>
2017-05-04 01:06:36 -0400

    active cus list

--- Xcinapex/src/servlet/MovieSugg.java
+++ Xcinapex/src/servlet/MovieSugg.java
@@ -11,6 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,6 +26,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Beans.Employee;
 import Beans.Movie;
 import utils.DBUtils;
 import utils.MyUtils;
@@ -37,6 +40,23 @@ public class MovieSugg extends HttpServlet {
     }
     
    	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   		HttpSession session = request.getSession(true);
+		//System.out.println(session.getAttribute("loggedInUser").getClass());
+		if (session.getAttribute("loggedInUser")!=null){//make sure someone is logged in to check
+			Object usertype = session.getAttribute("loggedInUser");
+			if(!(usertype instanceof Employee)) 
+			 {
+				RequestDispatcher dispatcher = request.getServletContext()
+		                .getRequestDispatcher("/WEB-INF/view/404EmpOnly.jsp");
+		        dispatcher.forward(request, response);
+			   return; //necessary to make the redirect happen right now
+			 }
+		}else if(session.getAttribute("loggedInUser")==null){
+			RequestDispatcher dispatcher = request.getServletContext()
+	                .getRequestDispatcher("/WEB-INF/view/404.jsp");
+	        dispatcher.forward(request, response);
+		   return;
+		}
    		RequestDispatcher dispatcher = request.getServletContext()
                 .getRequestDispatcher("/WEB-INF/view/MovieSuggestPerCust.jsp");
         dispatcher.forward(request, response);
