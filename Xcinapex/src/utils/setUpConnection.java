ec14c2fc2c7286f93c810c77365a9588cb2c9378
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-05-06 23:23:06 -0400

    moving repo

--- Xcinapex/src/utils/setUpConnection.java
+++ Xcinapex/src/utils/setUpConnection.java
@@ -15,32 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class setUpConnection {
-	/*
-	public static String SQLDUMP = "/usr/local/bin/mysqldump";
-	public static String DATABASENAME = "CineApex?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	public static String USERNAME = "manager";
-	public static String PASSWORD = "manager";
- */
-
-
-	/*
-	public static String DATABASENAME = "c305";
-	public static String USERNAME = "root";
-	public static String PASSWORD = "pass";
-<<<<<<< HEAD
-	 */
-
-	public static String DATABASENAME = "cinapex";
-	public static String USERNAME = "root";
-	public static String PASSWORD = "serverplz!";
-
-
-
-
-	 /*
-	public static String DATABASENAME = "cinapex";
-	public static String USERNAME = "root";
-	public static String PASSWORD = "serverplz!";
-	*/
+	
+	public static String DATABASENAME = "";
+	public static String USERNAME = "";
+	public static String PASSWORD = "";
 
 }
